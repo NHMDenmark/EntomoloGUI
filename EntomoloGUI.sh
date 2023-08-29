bf96107686e2aab4cb521011aa3ed0eea4794e72
Laurits Fredsgaard Larsen <laurits@fredsgaardlarsen.com>
2023-08-29 12:13:34 +0200

    run entomologui in sudo mode

--- EntomoloGUI.sh
+++ EntomoloGUI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /Users/dassco/miniconda3/bin/activate entomologui
 
-python /Users/dassco/Pi-Eyed-Piper/EntomoloGUI/main.py
+sudo python /Users/dassco/Pi-Eyed-Piper/EntomoloGUI/main.py
